52e6621c113fa00108ebf5f81cc7ab3868f7efa3
wh201906 <wh201906@yandex.com>
2021-01-14 20:32:24 +0800

    Add some code

---
+++ Verification_Reading_Simulations/Build_a_circuit_from_a_simulation_waveform/Circuit9.v
@@ -0,0 +1,15 @@
+module top_module (
+    input clk,
+    input a,
+    output [3:0] q );
+    
+    always@(posedge clk) begin
+        if(a)
+            q<=4'd4;
+        else if(q==4'd6)
+            q<=4'd0;
+        else
+            q<=q+4'd1;
+    end
+
+endmodule
